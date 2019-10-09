6bf46903ac505b4f24b63242d218a8410a02fe58
mikelev07 <45894165+mikelev07@users.noreply.github.com>
2019-10-09 13:51:34 +0300

    fix radiobuttons

---
+++ EQuiz/EQuiz/EQuiz/Models/RadioOption.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace EQuiz.Models
+{
+    public class RadioOption : INotifyPropertyChanged
+    {
+        public RadioCategory Category { get; }
+        public string Title { get; }
+        public string NameQuestion { get; }
+
+        private bool _isSelected { get; set; }
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (value != _isSelected)
+                {
+                    this._isSelected = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public RadioOption(RadioCategory category,string nameQuest, string title, bool isSelected = false)
+        {
+            this.NameQuestion = nameQuest;
+            this.Category = category;
+            this.Title = title;
+            this.IsSelected = isSelected;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public enum RadioCategory
+    {
+        CategoryA,
+        CategoryB,
+        CategoryC,
+        CategoryD
+    }
+}
--- EQuiz/EQuiz/EQuiz/Views/ItemsPage.xaml.cs
+++ EQuiz/EQuiz/EQuiz/Views/ItemsPage.xaml.cs
@@ -6,23 +6,90 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 using EQuiz.Models;
 using EQuiz.Views;
 using EQuiz.ViewModels;
+using System.Collections.ObjectModel;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace EQuiz.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
+ 
     public partial class ItemsPage : ContentPage
     {
-       
+
+        private ObservableCollection<Models.Grouping<string, RadioOption>> RadioOptionsList = new ObservableCollection<Models.Grouping<string, RadioOption>>();
+
+
         public ItemsPage()
         {
             InitializeComponent();
 
+           
+
+            Initialize();
+        }
+
+        public async void Handle_Clicked(object sender, EventArgs e)
+        {
+            await this.DisplayAlert("", "Ваш выбор сохранен (нет)", "OK");
+           
+        }
+
+        public void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            
+            ListView_Radio.SelectedItem = null;
+        }
+
+        public void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var item = e.Item as RadioOption;
+
+            if (item == null)
+                return;
+
+        
+            foreach (var group in RadioOptionsList)
+            {
+                if (group.Contains(item))
+                {
+                    foreach (var s in group.Where(x => x.IsSelected))
+                    {
+                        s.IsSelected = false;
+                    }
+
+                    item.IsSelected = true;
+                }
+            }
+        }
+
+        private void Initialize()
+        {
+            // Build a list of items
+            var items = new List<RadioOption>()
+            {
+                new RadioOption(RadioCategory.CategoryA,"Ты дурак?", "Чо куришь?", true),
+                new RadioOption(RadioCategory.CategoryA,"Ты дурак?", "Оранжевый"),
+                new RadioOption(RadioCategory.CategoryA,"Ты дурак?", "Ну нахер"),
+
+                new RadioOption(RadioCategory.CategoryB,"Ты мудак?", "Marvel", true),
+                new RadioOption(RadioCategory.CategoryB,"Ты мудак?", "DC"),
+
+
+                new RadioOption(RadioCategory.CategoryC,"Ты судак?", "Курица", true),
+                new RadioOption(RadioCategory.CategoryC,"Ты судак?", "БОранина"),
+             
+            };
+
+            var sorted = from item in items
+                         group item by item.NameQuestion into radioGroups
+                         select new Models.Grouping<string, RadioOption>(radioGroups.Key.ToString(), radioGroups);
+
+      
+            RadioOptionsList = new ObservableCollection<Models.Grouping<string, RadioOption>>(sorted);
+            ListView_Radio.ItemsSource = RadioOptionsList;
         }
 
     }
